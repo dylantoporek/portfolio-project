8d488dc8314c196cc0dcacc75eb9af5659e1ce8e
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-09 17:13:51 -0400

    updates

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -9,15 +9,18 @@ function Home(){
             </div>
 
             <div id='home-about'>
-                About snippit placeholder
+                About snippit placeholder... for more information:
+                <a href="/about">To About</a>
             </div>
 
             <div id='home-projects'>
-                Project snippit placeholder
+                Project snippit placeholder... for more information follow this link:
+                <a href="/portfolio">To Portfolio</a>
             </div>
 
             <div id='home-blogs'>
-                Blog snippit placeholder
+                <p>Blog snippit placeholder....To check out the blogs I have written please follow this link:</p>
+                <a href='https://medium.com/@dylantoporek'>https://medium.com/@dylantoporek</a>
             </div>
 
             <div id='contact-bar'>
