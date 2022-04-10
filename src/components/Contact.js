f79533e653ff07cff8e4face11bdc3d152fd2777
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-10 16:53:53 -0400

    contact links added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,9 +3,9 @@ import React from "react";
 function Contact(){
     return (
         <div id='contact-container'>
-            <p className="contact-item">Github</p>
-            <p className="contact-item">Medium</p>
-            <p className="contact-item">Linkedin</p>
+            <a className="contact-item" href='https://github.com/dylantoporek'>Github</a>
+            <a className="contact-item" href='https://medium.com/@dylantoporek'>Medium</a>
+            <a className="contact-item" href='https://www.linkedin.com/in/dylan-toporek-bb3491106/'>Linkedin</a>
         </div>
     )
 }
