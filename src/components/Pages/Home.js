424804b8be098cb7a9d0ebf355de56f870b5910c
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-12 16:24:41 -0400

    home color palette updates

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -14,18 +14,18 @@ function Home({changePage}){
 
             <div id='home-about'>
                Hi, my name is Dylan Toporek. I am a fullstack software engineer focusing on developing and designing exceptional web experiences. Currently, I am honing my skills using React and Ruby on Rails. For more information:
-                <a href="/about">To About</a>
+                <a className='home-link' href="/about">To About</a>
                 
             </div>
 
             <div id='home-projects'>
                 Project snippit placeholder... for more information follow this link:
-                <a href="/portfolio">To Portfolio</a>
+                <a className='home-link' href="/portfolio">To Portfolio</a>
             </div>
 
             <div id='home-blogs'>
                 <p>Blog snippit placeholder....for more information follow this link:</p>
-                <a href='https://medium.com/@dylantoporek'>Medium</a>
+                <a className='home-link' href='https://medium.com/@dylantoporek'>Medium</a>
             </div>
 
             <div id='contact-bar'>
