25650d9212b3187de653e0317a8e1c188940d04a
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-14 12:52:39 -0400

    contact logo's added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
 import React from "react";
+import githubImg from '../images/GithubLogo.png'
+import mediumImg from '../images/mediumLogo.png'
+import linkedinImg from '../images/linkedinLogo.webp'
 
 function Contact(){
     return (
         <div id='contact-container'>
-            <a className="contact-item" href='https://github.com/dylantoporek'>Github</a>
-            <a className="contact-item" href='https://medium.com/@dylantoporek'>Medium</a>
-            <a className="contact-item" href='https://www.linkedin.com/in/dylan-toporek-bb3491106/'>Linkedin</a>
+            <a className="contact-item" href='https://github.com/dylantoporek'>
+                <img className='logo-link' src={githubImg}></img>
+            </a>
+            <a className="contact-item" href='https://medium.com/@dylantoporek'>
+                <img className='logo-link' src={mediumImg}></img>
+            </a>
+            <a className="contact-item" href='https://www.linkedin.com/in/dylan-toporek-bb3491106/'>
+                <img className='logo-link' src={linkedinImg}></img>
+            </a>
         </div>
     )
 }
