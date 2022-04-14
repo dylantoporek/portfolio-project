ba411c17ef5a7af9a0b7ee8fefec08a5c4a13632
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-14 12:13:10 -0400

    link decoration check updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,15 @@ function Navbar({selectedPage}){
 
     return (
         <div id='navbar-container'>
-            {selectedPage === 'http://localhost:3000/' ? 
+            {selectedPage === 'http://localhost:3000/' || selectedPage === 'https://dylantoporek.netlify.app/' ? 
             <NavLink id='selected-to-home' to='/' onClick={handleScrollToTop}>Home</NavLink>
             :
             <NavLink className='navLink' to='/' onClick={handleScrollToTop}>Home</NavLink>}
-            {selectedPage === 'http://localhost:3000/portfolio' ? 
+            {selectedPage === 'http://localhost:3000/portfolio' || selectedPage === 'https://dylantoporek.netlify.app/portfolio' ? 
             <NavLink id='selected-to-portfolio' to='/portfolio' onClick={handleScrollToTop}>Portfolio</NavLink>
             :
             <NavLink className='navLink' to='/portfolio' onClick={handleScrollToTop}>Portfolio</NavLink>}
-            {selectedPage === 'http://localhost:3000/about' ?
+            {selectedPage === 'http://localhost:3000/about' || selectedPage === 'https://dylantoporek.netlify.app/about' ?
             <NavLink id='selected-to-about' to='/about' onClick={handleScrollToTop}>About</NavLink>
             :
             <NavLink className='navLink' to='/about' onClick={handleScrollToTop}>About</NavLink>}
