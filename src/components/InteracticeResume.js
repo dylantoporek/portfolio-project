6fb344528b992b2913631a6ab65d60138a4f472e
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-04-15 11:55:48 -0400

    interactive resume updates

--- src/components/InteracticeResume.js
+++ src/components/InteracticeResume.js
@@ -11,78 +11,86 @@ function InteractiveResume(){
                 'detail 2',
                 'detail 3',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '04/2022 - finish mm/year'
         },
         {
             company: 'Flatiron School Software Engineering Bootcamp',
             jobTitle: 'Student',
             details: [
-                'detail 1',
-                'detail 2',
-                'detail 3',
+                'Full Stack Web Development, Ruby on Rails and JavaScript programming',
+                'Experience with React for frontend development and Ruby on Rails for backend development',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '09/2021 - 03/2022'
         },
         {
             company: 'Private Tutor / Home School Teacher',
             jobTitle: 'Tutor / Teacher',
             details: [
-                'detail 1',
-                'detail 2',
-                'detail 3',
+                'Offered academic support in various subjects',
+                'Facilitated contact with school teachers and administration to ensure seamless schooling experience',
+                'Ensured maintenance of school work in students unable to attend due to illness',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '09/2017 - Present'
         },
         {
             company: 'Mathnasiun',
             jobTitle: 'Instructor',
             details: [
-                'detail 1',
-                'detail 2',
-                'detail 3',
+                'Math instructor at busy after school mathematics instruction company',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '09/2016 - Present'
         },
         {
             company: 'The Collegiate School',
-            jobTitle: 'Teacher',
+            jobTitle: 'Part-Time Teacher',
             details: [
-                'detail 1',
-                'detail 2',
-                'detail 3',
+                'Math teacher of two 7th grade classes',
+                'Prepared all course work, classroom activities, and assessments',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '01/2020 - 06/2020'
         },
         {
             company: 'The Birch Wathen Lenox School',
-            jobTitle: 'Contractor',
+            jobTitle: 'Assistant Teacher',
             details: [
-                'detail 1',
-                'detail 2',
-                'detail 3',
+                'Assistant math teacher / permanent substitute for maternity leave teacher',
+                'Assisted with four sections by crafting lessons, maintaining classroom order, and graded assignments',
             ],
-            dates: 'start mm/year - finish mm/year'
+            dates: '01/2017 - 06/2017'
         }, 
     ]
 
     function handleResumeClick(e){
         setShowExperienceDetials(e.target.value)
-        console.log(showExperienceDetails)
     }
-    
+
+    const workButtonsDisplay = experienceDetails.map((experience)=>{
+        return <button value={experience.company} onClick={handleResumeClick}>{experience.company}</button>
+    })
+
+    const targetedExperience = experienceDetails.find((experience) => {
+        return experience.company === showExperienceDetails
+    })
+
+    const workDetailsDisplay = <div>
+        <h4>{targetedExperience.jobTitle}</h4>
+        {targetedExperience.details[0] ? <li>{targetedExperience.details[0]}</li> : null}
+        {targetedExperience.details[1] ? <li>{targetedExperience.details[1]}</li> : null}
+        {targetedExperience.details[2] ? <li>{targetedExperience.details[2]}</li> : null}
+        <p>{targetedExperience.dates}</p>
+    </div>
+
     return (
         <div id='resume-container'>
                 <h4>Experience:</h4>
                 
                 <div id='work-container'>
                 <div id='home-experience-container'>
-                    <button value='Expedience' onClick={handleResumeClick}>Contractor at Expedience Software</button>
-                    <button value='Flatiron' onClick={handleResumeClick}>Flatiron School Software Engineering Bootcamp</button>
-                    <button value='Teacher' onClick={handleResumeClick}>Math Teacher</button>
+                    {workButtonsDisplay}
                 </div>
 
                 <div id='work-details'>
-                    {showExperienceDetails }
+                    {workDetailsDisplay}
                 </div> 
 
                 </div>
