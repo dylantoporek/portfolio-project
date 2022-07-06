86bf1d537cd2a3581757330f2a2a82a3bd377781
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-06 07:23:00 -0400

    images added for portfolio

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
+import botw from '../images/botw.png'
+import nintendo from '../images/nintendo.png'
+import lactate from '../images/lactate_1.png'
+import poke from '../images/poke.png'
+
 
 function ProjectItem(){
     const [projects, setProjects] = useState([
        {
             title: 'Breath of the Wild Cooking App',
-            gif: 'TBD',
+            image: botw,
             description: 'A cooking mini-game simulating an online shop with a cart and checkout. Buy ingredients and cook them!',
             link: 'https://obscure-scrubland-39099.herokuapp.com/',
             frontend: 'https://github.com/dylantoporek/botw-recipe-app', 
@@ -13,7 +18,7 @@ function ProjectItem(){
         }, 
         {
             title: 'Nintendo-Land',
-            gif: 'TBD',
+            image: nintendo,
             description: 'A board game where you race to the finish against three computer opponents.',
             link: 'https://frozen-eyrie-81829.herokuapp.com/',
             frontend: 'https://github.com/dylantoporek/Nintendo-Land', 
@@ -22,7 +27,7 @@ function ProjectItem(){
         },
         {
             title: 'Enduracne Lactate Analyzer',
-            gif: 'TBD',
+            image: lactate,
             description: 'A conceptual fitness app for althetes to track and measure their blood lactate levels while training.',
             link: 'https://serene-spire-65012.herokuapp.com/',
             frontend: 'https://github.com/jonathonmcclen/endurance-lactate-analyzer-client', 
@@ -31,7 +36,7 @@ function ProjectItem(){
         },
         {
             title: 'Pokemon Minigame App',
-            gif: 'TBD',
+            image: poke,
             description: 'A collection of Pokemon themed mini-games. Race on the track or battle in the arena!',
             link: 'https://tranquil-scrubland-82540.herokuapp.com/',
             frontend: 'https://github.com/dylantoporek/pokemon-mini-game-project', 
@@ -45,6 +50,7 @@ function ProjectItem(){
         return (
             <div className="project-item" key={project.title}>
                 <h3>{project.title}</h3>
+                <img className='project-image' src={project.image}/>
                 <p>{project.description}</p>
                 <div id='project-links-container'>
                     <a className="project-link" href={project.link}>Demo</a>
