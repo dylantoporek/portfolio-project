c2e4e6d744598abe583c29f322e91ccaaab450f7
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-11 15:11:23 -0400

    contact icons updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,18 +2,21 @@ import React from "react";
 import githubImg from '../images/GithubLogo.png'
 import mediumImg from '../images/mediumLogo.png'
 import linkedinImg from '../images/linkedinLogo.webp'
+import gitHub from '../images/github-brands.svg'
+import medium from '../images/medium-brands.svg'
+import linkedin from '../images/linkedin-in-brands.svg'
 
 function Contact(){
     return (
         <div id='contact-container'>
             <a className="contact-item" href='https://github.com/dylantoporek'>
-                <img className='logo-link' src={githubImg}></img>
+                <img className='logo-link' src={gitHub}></img>
             </a>
             <a className="contact-item" href='https://medium.com/@dylantoporek'>
-                <img className='logo-link' src={mediumImg}></img>
+                <img className='logo-link' src={medium}></img>
             </a>
             <a className="contact-item" href='https://www.linkedin.com/in/dylan-toporek-bb3491106/'>
-                <img className='logo-link' src={linkedinImg}></img>
+                <img className='logo-link' src={linkedin}></img>
             </a>
 
 
