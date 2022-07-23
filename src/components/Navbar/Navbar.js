aeaba100c15c0b0e42bf391f95636716658488e7
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2022-07-23 14:29:29 -0400

    navbar animationadded

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,33 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import { NavLink } from "react-router-dom";
 import PageNav from "../PageNav/PageNav";
 import '../Navbar/index.scss'
 import logo from '../../images/logo.png'
 
 function Navbar({selectedPage}){
+    const [selectedNav, setSelectedNav] = useState(null)
+    const [animation, setAnimation] = useState(false)
     
     function handleClick(){
         const section = document.getElementById('home-container')
         section.scrollIntoView( { behavior: 'smooth', block: 'start' } )
+        setSelectedNav(null)
     }
 
-    // function handleScrollToTop(){
-    //     window.scrollTo(0, 0)
-    // }
+    function handleNavSelection(props){
+        setSelectedNav(props)
+    }
 
-    // return (
-    //     <div id='navbar-container'>
-    //         {selectedPage === 'http://localhost:3000/' || selectedPage === 'https://dylantoporek.netlify.app/' ? 
-    //         <NavLink id='selected-to-home' to='/' onClick={handleScrollToTop}>Home</NavLink>
-    //         :
-    //         <NavLink className='navLink' to='/' onClick={handleScrollToTop}>Home</NavLink>}
-    //         {selectedPage === 'http://localhost:3000/portfolio' || selectedPage === 'https://dylantoporek.netlify.app/portfolio' ? 
-    //         <NavLink id='selected-to-portfolio' to='/portfolio' onClick={handleScrollToTop}>Portfolio</NavLink>
-    //         :
-    //         <NavLink className='navLink' to='/portfolio' onClick={handleScrollToTop}>Portfolio</NavLink>}
-    //         <a className="navLink" href='https://drive.google.com/file/d/1APNgek--JsLy05tjixRrRFtrf09HVQsY/view?usp=sharing'>Resume</a>
-    //     </div>
-    // )
+    function handleAnimation(props){
+        if(props === 'on'){
+            setAnimation(true)
+        } else {
+            setAnimation(false)
+        }
+    }
+
 
     return (
         <div id='navbar-container'>
@@ -40,7 +38,11 @@ function Navbar({selectedPage}){
                 zIndex: 10,
                 }} src={logo}/>
             </div>
-            <PageNav/>
+            <PageNav 
+            animation={animation} 
+            handleAnimation={handleAnimation}
+            selectedNav={selectedNav}
+            handleNavSelection={handleNavSelection}/>
         </div>
         
     )
--- src/components/PageNav/PageNav.js
+++ src/components/PageNav/PageNav.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from "react";
 import '../PageNav/index.scss'
 
-function PageNav(){
-    const [selectedNav, setSelectedNav] = useState(null)
+function PageNav({selectedNav, handleNavSelection, handleAnimation, animation}){
+    // const [selectedNav, setSelectedNav] = useState(null)
     
     const openInNewTab = url => {
         window.open(url, '_blank', 'noopener,noreferrer');
@@ -11,6 +11,15 @@ function PageNav(){
 
 
     function findMySection(e){
+        console.log(selectedNav, e.target.id)
+        if(selectedNav){
+            const wasSelected = document.getElementById(`${selectedNav}-deco`)
+            console.log(wasSelected)
+            wasSelected.className = 'pag-nav-deco-undo'
+            console.log(wasSelected)
+        }
+        handleNavSelection(e.target.id)
+        handleAnimation('on')
             if(e.target.id === 'About'){
                 const section = document.querySelector( '#home-about' );
                 section.scrollIntoView( { behavior: 'smooth', block: 'start' } );
@@ -25,28 +34,36 @@ function PageNav(){
             }
     }
 
+
+    // <div className='pag-nav-deco-undo'></div>
     const pageNavHelper = <div id='page-nav-container'>
         <div>
             <a className="page-nav" id='About' onClick={findMySection}>About</a>
-            {selectedNav === 'About' ? <div className='pag-nav-decoration'></div> : null}
+            {selectedNav === 'About' && animation !== false ? <div id="About-deco" className='pag-nav-decoration'></div> : null}
+            {selectedNav !== 'About' ? <div id='About-deco' className='pag-nav-deco-undo'></div> : null}
         </div>
 
         <div>
             <a className="page-nav" id='Experience' onClick={findMySection}>Experience</a>
-            {selectedNav === 'Experience' ? <div className='pag-nav-decoration'></div> : null}
+            {selectedNav === 'Experience' && animation !== false ? <div id="Experience-deco" className='pag-nav-decoration'></div> : null}
+            {selectedNav !== 'Experience' ? <div id='Experience-deco' className='pag-nav-deco-undo'></div> : null}
         </div>
 
         <div>
             <a className="page-nav" id='Projects' onClick={findMySection}>Projects</a>
-            {selectedNav === 'Projects' ? <div className='pag-nav-decoration'></div> : null}
+            {selectedNav === 'Projects' && animation !== false ? <div id="Projects-deco" className='pag-nav-decoration'></div> : null}
+            {selectedNav !== 'Projects'  ? <div id='Projects-deco' className='pag-nav-deco-undo'></div> : null}
         </div>
-        
+        <div>
         <a className="page-nav" id='resume-link' 
         target="_blank" 
         rel="noopener noreferrer"
         onClick={()=> openInNewTab('https://drive.google.com/file/d/1APNgek--JsLy05tjixRrRFtrf09HVQsY/view?usp=sharing')}>
             Resume
         </a>
+        {selectedNav !== 'Resume' ? <div id='Resume-deco' className='pag-nav-deco-undo'></div> : null}
+        </div>
+        
     </div>
     return (
         <div>
