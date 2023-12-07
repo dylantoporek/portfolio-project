229aac5c83e3b9498ab6ea9356e2ca67df5f8b24
dylantoporek <87386624+dylantoporek@users.noreply.github.com>
2023-12-07 10:04:07 -0500

    loading screen branch and files made

---
+++ src/components/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,9 @@
+import { Flex, Stack, Heading, Text, Image, useMediaQuery } from "@chakra-ui/react";
+import { motion } from "framer-motion"
+export default function LoadingScreen() {
+    return (
+        <motion.div>
+            
+        </motion.div>
+    )
+}
